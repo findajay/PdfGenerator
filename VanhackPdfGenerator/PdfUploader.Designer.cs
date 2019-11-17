a93b5b80ecd5dc2501aa878234460d54cf550ab2
Ajay Kumar <Ajay.Kumar@condecosoftware.com>
2019-11-17 17:59:55 +0530

    VAN-3 |  Implement Model pre processor to get data from database

---
+++ VanhackPdfGenerator/PdfUploader.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace VanhackPdfGenerator
+{
+    partial class PdfUploader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRender = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.srcPath = new System.Windows.Forms.Label();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputPath = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRender
+            // 
+            this.btnRender.Location = new System.Drawing.Point(295, 171);
+            this.btnRender.Name = "btnRender";
+            this.btnRender.Size = new System.Drawing.Size(130, 32);
+            this.btnRender.TabIndex = 4;
+            this.btnRender.Text = "Generate pdf";
+            this.btnRender.UseVisualStyleBackColor = true;
+            this.btnRender.Click += new System.EventHandler(this.btnRender_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(33, 50);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(115, 35);
+            this.btnBrowse.TabIndex = 5;
+            this.btnBrowse.Text = "Browse source pdf";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.browseSrc_Click);
+            // 
+            // srcPath
+            // 
+            this.srcPath.AutoSize = true;
+            this.srcPath.Location = new System.Drawing.Point(227, 61);
+            this.srcPath.Name = "srcPath";
+            this.srcPath.Size = new System.Drawing.Size(0, 13);
+            this.srcPath.TabIndex = 6;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 33);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Browse output pdf";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.browseOut_Click);
+            // 
+            // outputPath
+            // 
+            this.outputPath.AutoSize = true;
+            this.outputPath.Location = new System.Drawing.Point(192, 124);
+            this.outputPath.Name = "outputPath";
+            this.outputPath.Size = new System.Drawing.Size(0, 13);
+            this.outputPath.TabIndex = 8;
+            // 
+            // PdfUploader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 231);
+            this.Controls.Add(this.outputPath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.srcPath);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnRender);
+            this.Name = "PdfUploader";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnRender;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label srcPath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label outputPath;
+    }
+}
+
---
+++ VanhackPdfGenerator/PdfUploader.cs
@@ -0,0 +1,126 @@
+﻿using iText.Forms;
+using iText.Forms.Fields;
+using iText.Kernel.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VanhackPdfGenerator.DataProcessor;
+
+namespace VanhackPdfGenerator
+{
+    public partial class PdfUploader : Form
+    {
+        private static string SRCDocument = string.Empty;
+        private static string OUTDocument = string.Empty;
+        public PdfUploader()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Form pdf generate action click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRender_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var isTrue = DataFieldProcessor.ProcessPdfFormFields(SRCDocument, OUTDocument);
+                if (isTrue)
+                {
+                    MessageBox.Show("Pdf successfully rendered at output location. " + OUTDocument);
+                }
+                else
+                {
+                    MessageBox.Show("We are having some trouble in processing selected pdf.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        /// <summary>
+        /// Get browsed location for source pdf
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void browseSrc_Click(object sender, EventArgs e)
+        {
+            //To where your opendialog box get starting location. My initial directory location is desktop.
+            openFileDialog1.InitialDirectory = "C://Desktop";
+            //Your opendialog box title name.
+            openFileDialog1.Title = "Select file to be upload.";
+            //which type file format you want to upload in database. just add them.
+            openFileDialog1.Filter = "Select Valid Document(*.pdf;)|*.pdf;";
+            //FilterIndex property represents the index of the filter currently selected in the file dialog box.
+            openFileDialog1.FilterIndex = 1;
+            try
+            {
+                if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    if (openFileDialog1.CheckFileExists)
+                    {
+                        string path = System.IO.Path.GetFullPath(openFileDialog1.FileName);
+                        srcPath.Text = path;
+                        SRCDocument = path;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please Upload document.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Get browsed location for destination pdf
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void browseOut_Click(object sender, EventArgs e)
+        {
+            //To where your opendialog box get starting location. My initial directory location is desktop.
+            openFileDialog2.InitialDirectory = "C://Desktop";
+            //Your opendialog box title name.
+            openFileDialog2.Title = "Select file to be upload.";
+            //which type file format you want to upload in database. just add them.
+            openFileDialog2.Filter = "Select Valid Document(*.pdf;)|*.pdf;";
+            //FilterIndex property represents the index of the filter currently selected in the file dialog box.
+            openFileDialog2.FilterIndex = 1;
+            try
+            {
+                if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    if (openFileDialog2.CheckFileExists)
+                    {
+                        string path = System.IO.Path.GetFullPath(openFileDialog2.FileName);
+                        outputPath.Text = path;
+                        OUTDocument = path;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please Upload document.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
